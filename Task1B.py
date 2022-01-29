a632d3cd400f28f6604dbbf5a41e5de5df405c3c
Callum Reid <cmr67@cam.ac.uk>
2022-01-29 23:00:26 +0000

    1B

--- Task1B.py
+++ Task1B.py
@@ -1,9 +1,6 @@
 from floodsystem.geo import stations_by_distance
 from floodsystem.stationdata import build_station_list
 stations = build_station_list()
-print(stations_by_distance(stations, (52.2053, 0.1218))[:10])
-print(stations_by_distance(stations, (52.2053, 0.1218))[-10:])
-for x in stations_by_distance(stations, (52.2053, 0.1218))[:10]:
-    assert x[1] < 20
-for x in stations_by_distance(stations, (52.2053, 0.1218))[-10:]:
-    assert x[1] > 20
+p = (52.2053, 0.1218)
+print(stations_by_distance(stations, p)[:10])
+print(stations_by_distance(stations, p)[-10:])
