f1ed19fca63526661f3671972a94be2edd7c5434
Callum Reid <cmr67@cam.ac.uk>
2022-01-29 21:41:50 +0000

    Start of geo

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -6,4 +6,12 @@ geographical data.
 
 """
 
-from .utils import sorted_by_key  # noqa
+
+#from .utils import sorted_by_key  # noqa
+from haversine import haversine, Unit
+
+def stations_by_distance(stations, p):
+    for station in stations:
+        list_stations=[station.name, haversine(station.coordinates, p)]
+    return list_stations
+print(stations_by_distance(station.coordinates(23,12), p(12,23)))        
