dac19383f9e566e79919cc93dfe6874e01b71026
Callum Reid <cmr67@cam.ac.uk>
2022-01-30 17:25:32 +0000

    1B completed

--- Task1B.py
+++ Task1B.py
@@ -4,20 +4,22 @@ from floodsystem.stationdata import build_station_list
 p = (52.2053, 0.1218)
 
 def run():
-    # shell for 1B code to run
+    # Shell for 1B code to run
+    # Builing the lists of stations and the distance of stations from the coordinate p
     stations = build_station_list()
     station_dist = stations_by_distance(stations, p)
 
-    closest_ten = []
-    for station_d in station_dist [:10]:
+    closest_ten = [] # Empty list for the 10 closest stations
+    for station_d in station_dist [:10]:# Using the lowest 10 distances from p
         station, dist = station_d
-        closest_ten.append((station.name, station.town, dist))
+        closest_ten.append((station.name, station.town, dist)) # Putting the station name, town and distance in to the list
     
     print("")
-    print("The closest 10 stations are", closest_ten)
+    print("The closest 10 stations are", closest_ten) # Printing the closest 10 stations
     print("")
 
-    furthest_ten = []
+    # Same explaination as closest 10, but now furthest 10
+    furthest_ten = [] 
     for station_d in station_dist [-10:]:
         station, dist = station_d
         furthest_ten.append((station.name, station.town, dist))
