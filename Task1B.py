630dec4e825d4ae541e700f56e6cd3cdb951b0ea
Callum Reid <cmr67@cam.ac.uk>
2022-01-30 12:23:34 +0000

    Trying to add town

--- Task1B.py
+++ Task1B.py
@@ -1,6 +1,8 @@
 from floodsystem.geo import stations_by_distance
 from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import town
 stations = build_station_list()
+townlocation = town
 p = (52.2053, 0.1218)
-print(stations_by_distance(stations, p)[:10])
-print(stations_by_distance(stations, p)[-10:])
+print(stations_by_distance(stations, townlocation, p)[:10])
+print(stations_by_distance(stations, townlocation, p)[-10:])
