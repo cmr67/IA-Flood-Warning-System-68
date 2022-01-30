bd27221fbc638652592c71706299b729efb9f58d
Callum Reid <cmr67@cam.ac.uk>
2022-01-30 17:29:35 +0000

    1C completed

--- Task1C.py
+++ Task1C.py
@@ -5,19 +5,19 @@ centre = (52.2053, 0.1218)
 r = 10
 
 def run():
-    #shell for 1C code
-
+    # Shell for 1C code
+    # Builing the lists of stations and the stations within the radius r of the coordinates centre
     stations = build_station_list()
     stations_in_radius = stations_within_radius(stations, centre, r)
 
-    station_list = []
-    for station in stations_in_radius:
-        station_list.append(station.name)
+    station_list = [] # Empty list
+    for station in stations_in_radius: # The stations within the radius
+        station_list.append(station.name) # Append the stations name into the list
         
-    station_list.sort
+    station_list.sort # Sort the list
 
     for station in station_list:
-        print(station)
+        print(station) # Print the stations within the list
         print("")
 
 
