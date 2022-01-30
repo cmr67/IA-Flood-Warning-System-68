b580c11c5192938c1643acb83d6f151a5692ad46
Callum Reid <cmr67@cam.ac.uk>
2022-01-30 13:29:57 +0000

    1C continued

--- Task1C.py
+++ Task1C.py
@@ -1,5 +1,18 @@
 from floodsystem.geo import stations_within_radius
 from floodsystem.stationdata import build_station_list
-stations = stations_within_radius()
-centre = 52.2053, 0.1218
+
+centre = (52.2053, 0.1218)
 r = 10
+
+def run():
+    #shell for 1C code
+    stations = build_station_list
+    stations_in_radius = stations_within_radius(stations, centre, r)
+    station_list = []
+    for station in stations_in_radius:
+        station_list.append(station.name)
+
+    print(stations_in_radius)
+
+if __name__ == "__main__":
+    run()
