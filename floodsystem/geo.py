61f492540b9baebb2443c73009a904b67dae68af
Callum Reid <cmr67@cam.ac.uk>
2022-01-30 17:28:09 +0000

    geo completed up to 1C

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -23,6 +23,6 @@ def stations_within_radius(stations, centre, r):
     listradius = []
     for station in stations:
         distance_from_centre = haversine(station.coord, centre, unit=Unit.KILOMETERS) # Distance of the centre from the stations in km
-        if distance_from_centre <= r: # Ifd the distance is less than the specified radius, insclude it in the list
+        if distance_from_centre <= r: # If the distance is less than or equal to the specified radius, insclude it in the list
             listradius.append(station)
     return listradius
