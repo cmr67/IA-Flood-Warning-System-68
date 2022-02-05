21bac861d77b0dbb0170d73251713974e551351c
aa2314 <98557623+aa2314@users.noreply.github.com>
2022-02-05 17:24:27 +0000

    Update geo.py

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -26,3 +26,22 @@ def stations_within_radius(stations, centre, r):
         if distance_from_centre <= r: # If the distance is less than or equal to the specified radius, insclude it in the list
             listradius.append(station)
     return listradius
+#Task 1E
+def rivers_with_station(stations):
+    river=[]
+    for station in stations:
+        if station.river in river:
+            pass
+        else:
+            river.append(station.river)
+    return sorted_by_key(river)
+def stations_by_river(stations):
+    station_riv = {}
+    for station in stations:
+        if station.river in station_riv.keys()
+        station_riv[station.river].append(station.name)
+    else:
+        station_riv[station.river] = [station.name]
+    
+           
+    
