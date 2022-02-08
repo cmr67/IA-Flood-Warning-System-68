a35836b7550fea49da052befbc2c009b5f3f6169
Callum Reid <cmr67@cam.ac.uk>
2022-02-08 14:40:42 +0000

    Test update

--- test_station.py
+++ test_station.py
@@ -5,6 +5,7 @@
 
 from floodsystem.station import MonitoringStation
 from floodsystem.stationdata import build_station_list
+from floodsystem.station import inconsistent_typical_range_stations
 
 def test_create_monitoring_station():
 
@@ -25,4 +26,10 @@ def test_create_monitoring_station():
     assert s.typical_range == trange
     assert s.river == river
     assert s.town == town
-
+    
+def test_inconsistent_typical_range_stations():
+    stations = build_station_list()
+    x = inconsistent_typical_range_stations(stations)
+    for i in x:
+        assert type(i) == MonitoringStation
+    assert type(x) == list
