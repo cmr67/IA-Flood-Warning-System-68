1bf2fda2d8f5a403a1ee4baa7cbb0398afdde22e
aa2314 <98557623+aa2314@users.noreply.github.com>
2022-02-25 16:01:05 +0000

    Update Task2B.py

---
+++ Task2B.py
@@ -0,0 +1,19 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.flood import stations_level_over_threshold
+
+
+
+def run():
+    """Requirements for Task 2B"""
+
+    # Build list of stations
+    stations = build_station_list()
+
+    # Print number of stations
+
+
+if __name__ == "__main__":
+    print("")
+    print("*** Task 2B: CUED Part IA Flood Warning System ***")
+    print("")
+    run()
