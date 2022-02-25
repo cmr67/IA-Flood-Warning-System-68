584cccc4d1040a8ed9c91805d677ebbce15c1474
aa2314 <98557623+aa2314@users.noreply.github.com>
2022-02-25 13:40:04 +0000

    Create flood.py

---
+++ floodsystem/flood.py
@@ -0,0 +1,9 @@
+from .utils import sorted_by_key
+#implement a function that returns a list of tuples, where each tuple holds
+#(i) a station (object) at which the latest relative water level is over tol
+#(ii) the relative water level at the station:
+def stations_level_over_threshold(stations, tol):
+  x=[]
+  for station in stations:
+    if 
+  
