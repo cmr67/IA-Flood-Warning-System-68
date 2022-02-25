671f842649fd21c96f377950a884c3f614a0b6f5
aa2314 <98557623+aa2314@users.noreply.github.com>
2022-02-25 16:28:09 +0000

    Update Task2C.py

--- Task2C.py
+++ Task2C.py
@@ -1,1 +1,19 @@
-if
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.flood import stations_highest_rel_level
+
+def run():
+    """Requirements for Task 2C"""
+
+    # Build list of stations
+    stations = build_station_list()
+    # we want to print the first 10 when N = 10:
+    # before let's update the data:
+    update_water_levels(stations)
+    # now we print:
+    print(stations_highest_rel_level(stations, 10) 
+
+if __name__ == "__main__":
+    print("")
+    print("*** Task 2C: CUED Part IA Flood Warning System ***")
+    print("")
+    run()
