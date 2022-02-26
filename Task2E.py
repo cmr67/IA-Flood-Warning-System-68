1ee8be39e006cc670667fdaa8d453b5e4853ba7c
Callum Reid <cmr67@cam.ac.uk>
2022-02-26 14:22:10 +0000

    Task 2E updates

--- Task2E.py
+++ Task2E.py
@@ -2,6 +2,9 @@ from floodsystem.plot import plot_water_levels
 from datetime import datetime, timedelta
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_highest_rel_level
+from floodsystem.datafetcher import fetch_measure_levels
+import matplotlib
+import matplotlib.pyplot as plt
 
 def run():
     # Shell for 2E
@@ -9,8 +12,13 @@ def run():
     station = build_station_list
     update_water_levels(station)
     highest_relative_levels = stations_highest_rel_level(station, 5) # 5 stations with highest relative water levels
+    dt = 10 # Past 10 days
 
+    for station in highest_relative_levels:
+        dates, levels = fetch_measure_levels(station.measure_id, dt)
+        plot_water_levels(station, dates, levels)
 
+        plt.show()
 
 if __name__ == "__main__":
     print("")
