476c2185a90b589a301bf36c5bddc5d8e9d813af
Callum Reid <cmr67@cam.ac.uk>
2022-03-01 14:43:16 +0000

    Task 2E complete

--- Task2E.py
+++ Task2E.py
@@ -1,34 +1,35 @@
-from floodsystem.plot import plot_water_levels
-from datetime import datetime, timedelta
 from floodsystem.stationdata import build_station_list, update_water_levels
-from floodsystem.flood import stations_highest_rel_level
 from floodsystem.datafetcher import fetch_measure_levels
-import matplotlib.pyplot as plt
+from floodsystem.plot import plot_water_levels
+import datetime
+import matplotlib as plt
+from floodsystem.utils import sorted_by_key
 
 def run():
-    # Shell for 2E
-    # Updating all of the water levels for the stations
-    stations = build_station_list()
-    update_water_levels(stations)
-    highest_relative_levels = stations_highest_rel_level(stations, 5) # 5 stations with highest relative water levels
-    stations_plot = []
-    i = 0
-
-    dt = 10 # Past 10 days
-    for station in highest_relative_levels:
-        for i in stations:
-            if station[0] == [i]:
-                stations_plot.append[i]
-
-    for i in stations_plot:
-        dates, levels = fetch_measure_levels(i.measure_id, 
-                                dt=datetime.timedelta(days=dt)) # Fetching the updated water levels for the past 10 days
-                
-        plot_water_levels(i, dates, levels)# Shows the plot/results
-
-    
+    #Shell for Task 2E
+    stations = build_station_list() 
+    update_water_levels(stations) #Updating the water levels
+    High_Water = [] # Empty list
+    for station in stations:
+        if station.latest_level != None: # If the lastest water levels are not = to none 
+            #amend the station name and the water level into the list, else pass it
+            High_Water.append((station.name, station.latest_level))
+        else:
+            pass
+    sorted_list = sorted_by_key(High_Water, 1, True)[:5] # Sort the list for the 5 highest relative water levels
+    name_list = [] # Empty list
+    for i in range(5): # range for the 5 water stations that are the highest
+        name_list.append(sorted_list[i][0]) # append the sorted statioons into the list
+    print(sorted_list) # print the list
+    dt=10 # past 10 days
+    for station in stations:
+        for i in range(5):
+            if station.name == name_list[i]: # if the name of the station is in the station list
+                print(station.name) # print the station name
+                dates, levels = fetch_measure_levels(station.measure_id,dt = datetime.timedelta(dt))
+                plot_water_levels(station, dates, levels) # plot the station in that list for the past 10 days
+                # with the dates at the water levels for that station  
 
 if __name__ == "__main__":
-    print("")
     print("*** Task 2E: CUED Part IA Flood Warning System ***")
     run()
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -2,33 +2,21 @@ import matplotlib
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 from .analysis import polyfit
-from .stationdata import build_station_list
 import numpy as np
 
 def plot_water_levels(station, dates, levels):
-    stations=build_station_list()
-    # The high and low typical ranges
-    low_levels = station.typical_range[0]
-    high_level = station.typical_range[1]
-
-    # Plotting the current data and the typical high and low ranges
-    plt.plot(dates, levels, label = "Current startion data")
-    plt.plot(dates, low_levels, label = "Low Typical Water Level")
-    plt.plot(dates, high_level, label = "High Typical Water Level")
-
+    
+    plt.plot(dates, levels) # PLot for the dates and levels
     # Add axis labels, rotate date labels and add plot title
     plt.xlabel('Date')
     plt.ylabel('Water Level (m)')
     plt.xticks(rotation=45);
-    for station in stations:
-        plt.title("Water Level for {}" .format(station.name))
+    plt.title("Water Level for {}" .format(station.name))
 
     # Display plot
     plt.tight_layout()  # This makes sure plot does not cut off date labels
-    plt.legend()
     plt.show()
 
-    return
 
 def plot_water_level_with_fit(station, dates, levels, p):
     x = matplotlib.dates.date2num(dates)
