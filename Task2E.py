d2a86e6127040c23d70f9636d11ff7451eb2e0ed
Callum Reid <cmr67@cam.ac.uk>
2022-03-01 14:17:45 +0000

    Update

--- Task2E.py
+++ Task2E.py
@@ -3,7 +3,6 @@ from datetime import datetime, timedelta
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_highest_rel_level
 from floodsystem.datafetcher import fetch_measure_levels
-import matplotlib
 import matplotlib.pyplot as plt
 
 def run():
@@ -12,14 +11,22 @@ def run():
     stations = build_station_list()
     update_water_levels(stations)
     highest_relative_levels = stations_highest_rel_level(stations, 5) # 5 stations with highest relative water levels
+    stations_plot = []
+    i = 0
 
     dt = 10 # Past 10 days
-    for stations in highest_relative_levels:
-        station_data = stations[0]
-        dates, levels = fetch_measure_levels(station_data.measure_id, dt=datetime.timedelta(days=dt)) # Fetching the updated water levels for the past 10 days
-        plot_water_levels(station_data, dates, levels)
+    for station in highest_relative_levels:
+        for i in stations:
+            if station[0] == [i]:
+                stations_plot.append[i]
+
+    for i in stations_plot:
+        dates, levels = fetch_measure_levels(i.measure_id, 
+                                dt=datetime.timedelta(days=dt)) # Fetching the updated water levels for the past 10 days
+                
+        plot_water_levels(i, dates, levels)# Shows the plot/results
+
     
-        plt.show() # Shows the plot/results
 
 if __name__ == "__main__":
     print("")
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -2,9 +2,11 @@ import matplotlib
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 from .analysis import polyfit
+from .stationdata import build_station_list
 import numpy as np
 
 def plot_water_levels(station, dates, levels):
+    stations=build_station_list()
     # The high and low typical ranges
     low_levels = station.typical_range[0]
     high_level = station.typical_range[1]
@@ -18,7 +20,8 @@ def plot_water_levels(station, dates, levels):
     plt.xlabel('Date')
     plt.ylabel('Water Level (m)')
     plt.xticks(rotation=45);
-    plt.title("Water Level for" .format(station))
+    for station in stations:
+        plt.title("Water Level for {}" .format(station.name))
 
     # Display plot
     plt.tight_layout()  # This makes sure plot does not cut off date labels
@@ -39,7 +42,7 @@ def plot_water_level_with_fit(station, dates, levels, p):
     plt.xlabel('date')
     plt.ylabel('water level (m)')
     plt.xticks(rotation=45);
-    plt.title("Water Level Data and best-fit polynomial for" .format(station))
+    plt.title("Water Level Data and best-fit polynomial for" .format(station.measure_id))
 
     # Display plot
     plt.tight_layout()  # This makes sure plot does not cut off date labels
