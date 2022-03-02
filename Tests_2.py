dd1d841e3287759174e417ae7899aa6e327479db
Callum Reid <cmr67@cam.ac.uk>
2022-03-02 09:59:20 +0000

    Tests

---
+++ Tests_2.py
@@ -0,0 +1,10 @@
+from floodsystem.flood import stations_highest_rel_level
+from floodsystem.plot import plot_water_levels
+from floodsystem.plot import polyfit
+
+# Test 2B
+
+
+# Test 2E
+def test_plot_water_levles(station, dates, levels):
+    
