84dbbb5a1d4dca9b2b63d551f47df2d257773b4a
aa2314 <98557623+aa2314@users.noreply.github.com>
2022-03-02 11:03:47 +0000

    Update Tests_2.py

--- Tests_2.py
+++ Tests_2.py
@@ -2,9 +2,31 @@ from floodsystem.flood import stations_highest_rel_level
 from floodsystem.plot import plot_water_levels
 from floodsystem.plot import polyfit
 from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.station import MonitoringStation
+from floodsystem.flood import stations_level_over_threshold
+from floodsystem.flood import stations_highest_rel_level
 
 # Test 2B
-
+def test_for_2B():
+    s1 = MonitoringStation("test-s1-id", "test-m1-id", label= "station 1", coord = (-2.0, 4.0), trange= (0, 5), river= "River X", town= "My Town")
+    s2 = MonitoringStation("test-s2-id", "test-m2-id", label = "station 2", coord = (-2.0, 4.0), trange= (0,5), river= "River Y", town= "My Town")
+    s1.latest_level = 7
+    s2.latest_level = 3
+    stations_test=[s1,s2]
+    tol = 1
+    assert len(stations_level_over_threshold(stations_test, tol)) == 2
+    assert stations_level_over_threshold(stations_test, tol)[0] == ('station 3', 1.0)
+    assert stations_level_over_threshold(stations_test, tol)[1] == ('station 2', 0.6)
+#Test 2C
+def test_for_2C():
+    s1 = MonitoringStation("test-s1-id", "test-m1-id", label= "station 1", coord = (-2.0, 4.0), trange= (0, 5), river= "River X", town= "My Town")
+    s2 = MonitoringStation("test-s2-id", "test-m2-id", label = "station 2", coord = (-2.0, 4.0), trange= (0,5), river= "River Y", town= "My Town")
+    s1.latest_level = 7
+    s2.latest_level = 3
+    stations_test=[s1,s2]
+    assert len(stations_highest_rel_level(stations_test, length)) == 2
+    assert stations_highest_rel_level(stations_test, length)[0] == ('station 3', 1.0)
+    assert stations_highest_rel_level(stations_test, length)[1] == ('station 2', 0.6)
 
 # Test 2E
 def test_plot_water_levles(station, dates, levels):
